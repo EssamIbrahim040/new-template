5dc2aed8dbcb431ec72a418bd5d32b687de7cd29
Essam Ibrahim Muhammed <101215983+EssamIbrahim040@users.noreply.github.com>
2022-05-23 07:42:06 +0300

    Add files via upload

---
+++ my-website/js/index.js
@@ -0,0 +1,11 @@
+(window).on('load', function () {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item'
+    });
+    $('#portfolio-flters li').on( 'click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
+  
+      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+    });
+  });
